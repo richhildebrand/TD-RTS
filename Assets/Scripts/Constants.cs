5391b8cba2897a516c6cca38dca7286ca2a14cb9
HILDEBRANDR <HILDEBRANDR@TC-HILDEBRAND>
2019-01-08 15:10:49 -0700

    moving capsule!

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts
+{
+  public static class Constants
+  {
+    public static readonly int LeftMouseButton = 0;
+    public static readonly int RightMouseButton = 1;
+  }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+  public NavMeshAgent Agent { get; set; }
+
+  void Start()
+  {
+    Agent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(Constants.RightMouseButton))
+    {
+      Move(Agent);
+    }
+  }
+
+  private static void Move(NavMeshAgent agent)
+  {
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    Physics.Raycast(ray, out RaycastHit hit, 100);
+    agent.destination = hit.point;
+  }
+}
