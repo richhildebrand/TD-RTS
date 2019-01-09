8a56d0e042db8d3b416a32166c4c85e56d68a92f
HILDEBRANDR <HILDEBRANDR@TC-HILDEBRAND>
2019-01-09 13:07:23 -0700

    A* instead of navmesh

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,16 +1,17 @@
 ﻿using Assets.Scripts;
+using Pathfinding;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Unit : MonoBehaviour
 {
-  public NavMeshAgent Agent { get; set; }
+  public AIPath AiPath { get; set; }
   public Animator Animator { get; set; }
   public Constants.UnitState UnitState;
 
   void Start()
   {
-    Agent = GetComponent<NavMeshAgent>();
+    AiPath = GetComponent<AIPath>();
     Animator = GetComponent<Animator>();
   }
 
@@ -20,25 +21,15 @@ public class Unit : MonoBehaviour
     {
       Move(this);
     }
-
-    if (Agent.velocity == Vector3.zero && Constants.UnitState.Idle != UnitState)
-    {
-      UnitAnimator.Idle(Animator);
-      UnitState = Constants.UnitState.Idle;
-    }
-
-    if (Agent.velocity != Vector3.zero && Constants.UnitState.Moving != UnitState)
-    {
-      UnitAnimator.Move(Animator);
-      UnitState = Constants.UnitState.Moving;
-    }
   }
 
   private static void Move(Unit unit)
   {
     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
     Physics.Raycast(ray, out RaycastHit hit, 100);
-    unit.Agent.destination = hit.point;
+
+
+    unit.AiPath.destination = hit.point;
     Debug.Log("Move");
   }
 }
